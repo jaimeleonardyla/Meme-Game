fdaa7d9eda0930d1361a7f194980347db6ab2472
Juan Pablo Sáenz <jpsaenzmo@gmail.com>
2024-06-05 17:30:54 +0200

    Created the exam template

---
+++ server/index.mjs
@@ -0,0 +1,11 @@
+// imports
+import express from 'express';
+
+// init express
+const app = new express();
+const port = 3001;
+
+// activate the server
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
